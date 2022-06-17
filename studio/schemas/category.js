01013854040356909fe3d8813c67210ccc7d6e53
Novanda Ahsan <nathoilla@indo-soft.com>
2022-06-17 15:58:57 +0700

    add category schema

---
+++ studio/schemas/category.js
@@ -0,0 +1,22 @@
+export default {
+  name: "category",
+  type: "document",
+  title: "Category",
+  fields: [
+    {
+      name: "title",
+      type: "string",
+      title: "Title",
+    },
+    {
+      title: "Slug",
+      name: "slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96,
+        auto: true,
+      },
+    },
+  ],
+};
--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -2,8 +2,9 @@ import createSchema from "part:@sanity/base/schema-creator";
 import schemaTypes from "all:part:@sanity/base/schema-type";
 
 import article from "./article";
+import category from "./category";
 
 export default createSchema({
   name: "default",
-  types: schemaTypes.concat([article]),
+  types: schemaTypes.concat([article, category]),
 });
