3849723a6ea54f0e970c48b045a54d65e7f0ef72
Novanda Ahsan <nathoilla@indo-soft.com>
2022-06-17 16:59:40 +0700

    update card style and show post category title

--- web/src/components/Card.tsx
+++ web/src/components/Card.tsx
@@ -6,17 +6,21 @@ const Card: React.FC<{ post: any }> = ({ post }) => {
   const date = useMemo(() => formatDate(post?._createdAt), [post._createdAt]);
 
   return (
-    <div className="bg-zinc-800 p-[40px] shadow-sm cursor-pointer md:min-h-240 lg:min-h-320">
+    <div className="flex flex-col justify-between bg-zinc-800 p-[40px] shadow-sm cursor-pointer md:min-h-240 lg:min-h-320">
       <div>
         <span className="text-[10px] font-bold tracking-[3px] uppercase text-slate-200">
-          Blog
+          {post.category.title}
         </span>
-        <time className="block mt-[8px">{date}</time>
+        <time className="block mt-[3px] text-[14px] font-light text-zinc-400">
+          {date}
+        </time>
       </div>
-      <div className="mt-auto">
-        <h3 className="font-bold text-xl mt-auto pt-3">{post.title}</h3>
-        <Link href={'/'}>
-            <a className="mt-1">Details</a>
+      <div className="flex flex-col gap-3">
+        <h3 className="font-bold text-xl">{post.title}</h3>
+        <Link href={"/"}>
+          <a className="mt-4 relative block text-sm font-light leading-[3px] after:content-[''] after:absolute after:transform after:translate-x-2 after:translate-y-[2px] after:h-[1px] after:w-10 after:bg-white">
+            Details
+          </a>
         </Link>
       </div>
     </div>
